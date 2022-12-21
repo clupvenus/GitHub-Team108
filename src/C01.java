a5abca5dc6fb36f9d947ce0de8f04ce0345af009
Humeyra Uysal <humeyrasahinuysal@gmail.com>
2022-12-21 22:41:06 +0100

    v3

--- src/C01.java
+++ src/C01.java
@@ -2,6 +2,7 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("Hello github");
         System.out.println("Cmd cok kullanisli");
+        System.out.println("Bugunun sonunu gorebilecek miyiz");
 
     }
 }
